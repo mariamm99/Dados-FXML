f4786b7573611fdad1350bcfe3a78d8f53b67e9e
Rafa Caballero <rafacab.dev@gmail.com>
2020-05-29 16:21:21 +0200

    Creación de jugadores funcionando

--- risco/Main.java
+++ risco/Main.java
@@ -1,5 +1,7 @@
 package risco;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -9,26 +11,38 @@ import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
+  
+  public void pedirJugadores() throws IOException {
+    Stage stgJugadores = new Stage();
+    FXMLLoader fxmlJugadores = new FXMLLoader(this.getClass().getResource("vistas/NumeroDeJugadores.fxml"));
+    stgJugadores.setTitle("Risco: Introduce el número de jugadores");
+    Pane rootJugadores = fxmlJugadores.<Pane>load();
+    stgJugadores.setScene(new Scene(rootJugadores));
+    stgJugadores.setAlwaysOnTop(true);
+    stgJugadores.show();
+  }
+  
 	@Override
 	public void start(Stage primaryStage) {
-//		try {
-//		 FXMLLoader fxml = new FXMLLoader(this.getClass().getResource("vistas/NumeroDeJugadores.fxml"));
-//	    primaryStage.setTitle("Buscar un patrón");
-//	    Pane root = fxml.<Pane>load();
-//	    
-//	    primaryStage.setScene(new Scene(root));
-//			primaryStage.show();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
+    /* try {
+      FXMLLoader fxml = new FXMLLoader(this.getClass().getResource("vistas/NumeroDeJugadores.fxml"));
+      primaryStage.setTitle("Buscar un patrón");
+      Pane root = fxml.<Pane>load();
+      
+      primaryStage.setScene(new Scene(root));
+      primaryStage.show();
+    } catch(Exception e) {
+      e.printStackTrace();
+    } */
 	  
 	  //PARA REALIZAR MIENTRAS TIRAR DADOS
 	  try {
 	     FXMLLoader fxml = new FXMLLoader(this.getClass().getResource("vistas/risco.fxml"));
-	      primaryStage.setTitle("Buscar un patrón");
+	      primaryStage.setTitle("Risco");
 	      BorderPane root = fxml.<BorderPane>load();
 	      
 	      primaryStage.setScene(new Scene(root));
+	      pedirJugadores();
 	      primaryStage.show();
 	    } catch(Exception e) {
 	      e.printStackTrace();
--- risco/vistas/riscoController.java
+++ risco/vistas/riscoController.java
@@ -2,111 +2,142 @@ package risco.vistas;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 
-import java.awt.Button;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
-import gestisimal.Articulo;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import risco.Partida;
 
-public class riscoController {
-	@FXML
-	private MenuItem exportar;
-	@FXML
-	private MenuItem importar;
-	@FXML
-	private MenuItem ayuda;
-	@FXML
-	private TextField Jugador;
-	@FXML
-	private ImageView imgDado1;
-	@FXML
-	private ImageView imgDado2;
-	@FXML
-	private ImageView imgDado3;
-	
+public class riscoController implements Initializable {
+
+  Partida partida;
+
+  // risco.fxml //////
+  // MenuItem
+  @FXML
+  private MenuItem exportar;
+  @FXML
+  private MenuItem importar;
+  @FXML
+  private MenuItem ayuda;
+  // TextField
+  @FXML
+  private TextField jugador;
+  // ImageView
+  @FXML
+  private ImageView imgDado1;
+  @FXML
+  private ImageView imgDado2;
+  @FXML
+  private ImageView imgDado3;
+
+  // NumeroDeJugadores.fxml //////
+  // TextField
   @FXML
   private TextField nJugadores;
-  
+  @FXML
+  private Button aceptarNJugadores;
+
+  // NumDados.fxml //////
+  // TextField
   @FXML
   private TextField nDadosCambiar;
 
-  
-  Partida partida;
-
   @FXML
-  void ventanaNombreJugadores(ActionEvent event) {
-   
-    ArrayList<String> nombres=new ArrayList<String>();
+  void ventanaNombreJugadores(ActionEvent event) throws IOException {
 
-    List<TextField> text = new ArrayList<>();
-   
-   int numJugadores=Integer.parseInt(nJugadores.getText());
-   
-   //DECLARAR PARTIDA TRAS VER EL NUMERO DE JUGADORES. NO SE SI HACERLO ASI PERMITIRÁ LUEGO EL ACCESO DESDE OTRAS FUNCIONES
-   partida=new Partida(numJugadores);
-   
-   Stage stage = new Stage();
-   stage.setTitle("Nombre Jugadores");
+    // Cierro la ventana previa (Número de jugadores)
+    Stage stagenJugadores = (Stage) aceptarNJugadores.getScene().getWindow();
+    stagenJugadores.close();
 
-//   FXMLLoader fxml = new FXMLLoader(this.getClass().getResource("baja.fxml"));
-//
-//   Pane root = fxml.<Pane>load();
-   
-   
-   BorderPane root = new BorderPane();
-   VBox vbox = new VBox();
-   
-  for (int i = 0; i < numJugadores; i++) {
-    TextField textF = new TextField();
-    text.add(textF);
-  } 
-  
+    List<TextField> textfields = new ArrayList<TextField>();
 
-  //asi recorremos todos los textFields q necesitamos, podriamos utilizarlo de nombre textField0, textField1, pero no se como crearlos
-  for (TextField TextF : text) {
-    nombres.add(TextF.getText());
-    
+    int numJugadores = Integer.parseInt(nJugadores.getText());
+
+    // DECLARAR PARTIDA TRAS VER EL NUMERO DE JUGADORES.
+    partida = new Partida(numJugadores);
+
+    Stage stgnomJugadores = new Stage();
+    stgnomJugadores.setTitle("Risco: Introduce el número de jugadores");
+    stgnomJugadores.setAlwaysOnTop(true);
+
+    // Crear TextField y añadirlos a la lista de TextFields
+    for (int i = 1; i <= numJugadores; i++) {
+      TextField textF = new TextField("Jugador " + i);
+      textfields.add(textF);
+    }
+
+    Pane panel = new Pane();
+    panel.getChildren().add(new Label("Introduce el nombre de los jugadores: "));
+
+    Button btnAc = new Button("Comenzar");
+    btnAc.relocate(0, 20);
+    panel.getChildren().add(btnAc);
+
+    int contador = 1;
+    for (TextField tf : textfields) {
+      tf.relocate(0, 50 * contador);
+      panel.getChildren().add(tf);
+      contador++;
+    }
+
+    panel.setPadding(new Insets(20, 20, 20, 20));
+
+    stgnomJugadores.setScene(new Scene(panel));
+    stgnomJugadores.show();
+
+    btnAc.setOnAction(actionEvent -> {
+      int tmpCrearJugadores = 0;
+      for (TextField tf : textfields) {
+        tmpCrearJugadores++;
+        partida.crearJugadores(tmpCrearJugadores, tf.getText());
+      }
+      stgnomJugadores.close();
+    });
+
   }
-  
-  Button boton = new Button("iniciar partida");
-  
-  
-   //root.setCenter(vbox);
-   stage.setScene(new Scene(root));
-   stage.showAndWait();
-   
-    
-  }
 
-	// Event Listener on Button.onAction
-	@FXML
-	public void ventanaDados(ActionEvent event) throws IOException {
-	  Stage stage = new Stage();
+  // Event Listener on Button.onAction
+  @FXML
+  public void ventanaDados(ActionEvent event) throws IOException {
+    Stage stage = new Stage();
     stage.setTitle("Alta");
 
     FXMLLoader fxml = new FXMLLoader(this.getClass().getResource("NumDados.fxml"));
     Pane root = fxml.<Pane>load();
     stage.setScene(new Scene(root));
     stage.showAndWait();
-	}
-	
-	
+  }
+
   @FXML
   void cambiarDados(ActionEvent event) {
 
   }
-	
+
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+    
+  }
+
 }
